f03bc659a11d83b93558f6f4054352709c7adecb
jideabdqudus <jideabdqudus@gmail.com>
2021-07-30 11:08:12 +0100

    +

---
+++ src/components/forms/LoginForm.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Form, Input, Button, Row, Col } from "antd";
+import { Link } from "react-router-dom";
+
+
+
+export const LoginForm = () => {
+  return (
+    <div className="login">
+      <h3>Welcome Back.</h3>
+      <h5>
+        Login with your details below.
+      </h5>
+      <div>
+        <Form
+          name="normal_login">
+          <span>Email</span>
+          <Form.Item
+            name="email"
+            rules={[
+              { required: true, message: "Confirm your Input" },
+            ]}>
+            <Input
+              className="login__input"
+              placeholder="Enter email" type="email" name="email"
+            />
+          </Form.Item>
+          <span>Password</span>
+          <Form.Item
+            name="password"
+            rules={[
+              { required: true, message: "Confirm your Input" },
+            ]}
+          >
+            <Input.Password
+              className="login__input"
+              type="password" name="password" placeholder="Enter password"
+            />
+          </Form.Item>
+          <Row>
+            <Col xs={{ span: 12 }} lg={{ span: 24 }}>
+              <Form.Item>
+                <Button
+                  type="primary" htmlType="submit"                 className="login__btn"
+                >
+                  Login
+                </Button>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+        <p>
+          Don't have an account? <Link to="/signup"> Sign Up</Link>
+        </p>
+      </div>
+    </div>
+  )
+}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,16 +3,17 @@ import {Row, Col} from 'antd'
 
 //Imports
 import {assets} from "../assets/assets"
+import {LoginForm} from "../components"
 
 export const Login: React.FC= () => {
   return (
     <div className="auth">
       <Row className="auth__row">
-        <Col span={12} className="auth__bg">
+        <Col xs={{ span: 0 }} lg={{ span: 10 }} className="auth__bg">
           <img src={assets.logo} alt="Logo" width="100" />
         </Col>
-        <Col span={12}>
-          Hi
+        <Col xs={{ span: 20 }} lg={{ span: 14 }}>
+          <LoginForm/>
         </Col>
       </Row>
     </div>
