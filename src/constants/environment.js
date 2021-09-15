f85df8052ae0e9097c238db1dcdff096f980c727
jideabdqudus <jideabdqudus@gmail.com>
2021-09-15 10:45:35 +0100

    +

--- src/constants/environment.js
+++ src/constants/environment.js
@@ -1,6 +1,6 @@
 export const appConstants = {
   REACT_APP_BASE_URL:
     process.env.NODE_ENV === "development"
-      ? "https://trail-api.test.vggdev.com"
+      ? "https://trailapi1test.azurewebsites.net"
       : window.env.REACT_APP_BASE_URL,
 };
